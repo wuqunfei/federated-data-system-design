93a1da82dc53221b7b36f921ddb98ea0d3f46775
Qunfei Wu <wu.qunfei@gmail.com>
2025-11-23 16:49:49 +0100

    adding scripte

--- tests/run.trino.sh
+++ tests/run.trino.sh
@@ -2,5 +2,4 @@ docker run --name trino -d \
     -p 8080:8080 \
     --volume $PWD/etc:/etc/trino \
     --volume $PWD/data:/data/trino \
-    -e CATALOG_MANAGEMENT=dynamic \ 
-    trinodb/trino:latest
+    trinodb/trino:477
