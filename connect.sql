af7f049a365acefdf7f54dd6e93be1a0a98e6d1e
Qunfei Wu <wu.qunfei@gmail.com>
2025-11-24 13:12:35 +0100

    adding mcp

--- connect.sql
+++ connect.sql
@@ -2,5 +2,16 @@ INSTALL postgres;
 LOAD postgres;
 INSTALL snowflake;
 LOAD snowflake;
-INSTALL mysql
-LOAD mysql
+INSTALL mysql;
+LOAD mysql;
+
+
+
+-- Replace with your actual passwords or use environment variables
+-- Example: ATTACH 'postgres://avnadmin:${CAR_INSURANCE_PASSWORD}@customer-db-xllianz.e.aivencloud.com:26851/defaultdb' AS car_insurance (TYPE postgres, READ_ONLY);
+-- Example: ATTACH 'host=travel-insurance-xllianz.c.aivencloud.com user=avnadmin port=26851 password=${TRAVEL_INSURANCE_PASSWORD} database=defaultdb' AS travel_insurance (TYPE mysql, READ_ONLY);
+
+-- ATTACH 'postgres://avnadmin:YOUR_CAR_INSURANCE_PASSWORD@customer-db-xllianz.e.aivencloud.com:26851/defaultdb' AS car_insurance (TYPE postgres, READ_ONLY);
+-- ATTACH 'host=travel-insurance-xllianz.c.aivencloud.com user=avnadmin port=26851 password=YOUR_TRAVEL_INSURANCE_PASSWORD database=defaultdb' AS travel_insurance (TYPE mysql, READ_ONLY);
+-- CREATE SECRET my_snowflake (TYPE snowflake, ACCOUNT 'xhdfsdj-xg45373', USER 'qunfei', PASSWORD '2025@Munich@Darmstadt', DATABASE 'PC', WAREHOUSE  'COMPUTE_WH', SCHEMA 'HOUSE_INSURANCE');
+-- ATTACH '' AS house_insurance (TYPE snowflake, SECRET my_snowflake, READ_ONLY);
---
+++ run.mcp.sh
@@ -0,0 +1,5 @@
+uvx mcp-server-motherduck \
+    --transport sse \
+    --port 8000 \
+    --db-path federated.db \
+    --init-sql connect.sql
